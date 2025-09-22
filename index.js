ea0de0b99cfec6f8a788fa02a8ca6bbe08d08bfc
artisonii <artisoni2615@gmail.com>
2025-09-22 22:53:22 +0530

    crud done

---
+++ index.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const PORT = 5000;
+
+app.get("/", (req, res) => {
+  res.send("server is running");
+});
+
+// Add task
+
+const tasks = [
+  { id: 1, Title: "work", Description: "work is worship" },
+  { id: 2, Title: "work1", Description: "pooja is worship" },
+];
+
+app.post("/tasks", (req, res) => {
+  const { Title, Description } = req.body;
+  const newTask = { id: tasks.length + 1, Title, Description };
+  res.status(201).json({ message: "new task is created" });
+  res.send(newTask);
+});
+
+app.get("/tasks", (req, res) => {
+  res.send(tasks);
+});
+
+app.put("/tasks/:id", (req, res) => {
+  const task = tasks.find((t) => t.id === parseInt(req.params.id));
+  if (!task) {
+    res.status(404).json({ message: "task not found" });
+  }
+
+  task.Title = Title || task.Title;
+  task.Description = Description || task.Description;
+  res.send(task);
+});
+
+app.delete("tasks/:id", (req, res) => {
+  tasks = tasks.find((t) => t.id !== parseInt(req.params.id));
+  if (!task) {
+    res.json({ message: "task deleted succeesfully" });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`server is running on ${PORT}`);
+});
---
+++ readme.md
@@ -0,0 +1,17 @@
+Mock node interview
+
+- Start with - npm init-y
+- install all dependancy = npm i express cors nodemon.
+
+---
+
+- to add Task - route -http://localhost/tasks
+- to display task - http://localhost/tasks
+- to update task - http://localhost/tasks/:id
+- to display task - http://localhost/tasks:id
+
+- to add Task req.body
+  {
+  Title:"Anything:,
+  Description:"Anything
+  }
